d532630673dd9e1f34440021279050bd26948ef1
Spyro1001 <raymondhedgecock@gmail.com>
2024-08-27 19:29:52 -0700

    algorithms

---
+++ src/main/java/com/sandbox/algorithm/FibonacciNumbers.java
@@ -0,0 +1,48 @@
+package com.sandbox.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FibonacciNumbers {
+
+    private Map<Integer, Long> dictionary = new HashMap<>();
+
+    public static void main(String[] args) {
+
+            FibonacciNumbers fn = new FibonacciNumbers();
+
+            for (int n = 0; n <= 92; n++) {
+                System.out.println("fib(" + n + ") = " + (fn.fib(n)));
+            }
+    }
+
+    public long fib(int x) {
+
+        // f0 = 0
+        if (x == 0) {
+            return 0L;
+        }
+
+        // f1 = 1
+        if (x == 1) {
+            return 1L;
+        }
+
+        // fn = fn-1 + fn-2
+        return memo(x - 1) + memo(x - 2);
+    }
+
+    public long memo(int m) {
+
+        Long value = dictionary.get(m);
+
+        if (value != null) {
+            return value;
+        }
+
+        value = fib(m);
+        dictionary.put(Integer.valueOf(m), value);
+        return value;
+    }
+
+}
